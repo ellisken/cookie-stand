c1da65eeff478aa000a3c723093cb52f5995754a
ellisken <ellisken@oregonstate.edu>
2018-08-06 13:18:35 -0700

    implemented customersPerHour() and salesPerHour()'

--- js/sales.js
+++ js/sales.js
@@ -19,20 +19,22 @@ Display the values of each array as unordered lists in the browser*/
 
 
 /*********************************************************
- *           Define function for customers per hour
- *           and sales projections
+ *                     Define functions
  *********************************************************/ 
+// Generates a random whole number of customers
+// within the range of [minCust, maxCust]
+ function customersPerHour(){
+  return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+}
+
+
 // Calculates projected sales = 
 // average cookies per customer * # of customers
- function customersPerHour(){
-
+ function salesPerHour(){
+  return this.avgCookiesPerCust * this.customersPerHour();
 }
 
 
-
- function salesPerHour(hour){
-
-}
 
 
 /*********************************************************
@@ -42,38 +44,38 @@ Display the values of each array as unordered lists in the browser*/
   minCust: 23,
   maxCust: 65,
   avgCookiesPerCust: 6.3,
-  hourlyCustomers:,
-  salesPerHour:
+  hourlyCustomers: customersPerHour,
+  salesPerHour: salesPerHour
  }
 
  var seatac = {
   minCust: 3,
   maxCust: 24,
   avgCookiesPerCust: 1.2,
-  hourlyCustomers:
-  salesPerHour:
+  hourlyCustomers: customersPerHour,
+  salesPerHour: salesPerHour
  }
 
  var seattleCenter = {
   minCust: 11,
   maxCust: 38,
   avgCookiesPerCust: 3.7, 
-  hourlyCustomers:
-  salesPerHour:
+  hourlyCustomers: customersPerHour,
+  salesPerHour: salesPerHour
  }
 
  var capitolHill = {
   minCust: 20,
   maxCust: 38,
-  avgCookiesPerCust: 
-  hourlyCustomers:
-  salesPerHour:
+  avgCookiesPerCust: 2.3
+  hourlyCustomers: customersPerHour,
+  salesPerHour: salesPerHour
  }
 
  var alki = {
   minCust: 2,
   maxCust: 16,
   avgCookiesPerCust: 4.6 
-  hourlyCustomers:
-  salesPerHour:
+  hourlyCustomers: customersPerHour,
+  salesPerHour: salesPerHour
  }
